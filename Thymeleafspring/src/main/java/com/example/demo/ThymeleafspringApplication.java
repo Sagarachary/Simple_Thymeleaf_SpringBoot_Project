46610f371344728dc1bac240e5d0720e725127ec
Sagarachary <acharysagar01@gmail.com>
2025-12-24 12:15:52 +0530

    Simple Thymeleaf depencies SpringBoot project

---
+++ Thymeleafspring/src/main/java/com/example/demo/ThymeleafspringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafspringApplication.class, args);
+	}
+
+}
---
+++ Thymeleafspring/src/test/java/com/example/demo/ThymeleafspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
